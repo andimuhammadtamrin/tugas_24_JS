30264592bc9cfb308674e9d8019165d76e8d5a29
Andi Muhammad Tamirn <andimuhammadtamrin@gamil.com>
2019-08-19 22:45:59 +0800

    Tugas 24 Java Script
    Tugas ini saya kumpulkan untuk Niomic

---
+++ tugas24.js
@@ -0,0 +1,9 @@
+function deret(){
+  var angka = [32,33,16,40,9,7,11,2,44,1,66];
+  console.log("Sebelumnya :",angka);
+  console.log("Ascending :",angka.sort());
+  console.log("Descending :",angka.reverse());
+
+
+}
+deret()
